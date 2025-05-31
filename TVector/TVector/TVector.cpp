041755b0715c1125239ad61ec6e6fcf055db0d34
Polina0013 <p.pozdnova@gmail.com>
2025-05-31 22:05:05 +0300

    Add the test system

--- TVector/TVector/TVector.cpp
+++ TVector/TVector/TVector.cpp
@@ -1,1 +1,2 @@
 // Copyright 2025 Pozdnova Polina
+#include "TVector.h"
--- TVector/TVector/TVector.h
+++ TVector/TVector/TVector.h
@@ -1,7 +1,18 @@
 // Copyright 2025 Pozdnova Polina
 #pragma once
 
+#include <iostream>
+#include <utility>
+#include <random>
+#include <chrono>
+#include <initializer_list>
+
+#define CAPACITY 15
+#define DELETED_LIMIT 0.15
+
+enum State { empty, busy, deleted };
+
 class TVector {
-	int a;
-	int abc;
+    int a;
+    int abc;
 };
